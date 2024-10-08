2df2692fa4c5960aa199ce29d0f426487f1ba718
KishanBGajera <kishangajera1411@gmail.com>
2024-10-08 18:05:02 +0530

    modify index and package

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 
-require("./utils/db");
+require("./DB/connect.db");
 
 app.use(express.json());
-// app.use(
-//     cors({
-//         origin: "*",
-//     })
-// );
 
 // CORS middleware
 app.use(cors({
@@ -22,17 +17,8 @@ app.use(cors({
     credentials: true
 }));
 
-// Custom CORS headers
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-// });
-
 app.use(cookieParser());
-app.use("/api", require("./routes/index"))
+app.use("/api", require("./routes/routers.routes"))
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
