a4c29f9ac45569dcc939f6087c6038749cf3d457
Dhruvin Malani <malanidhruvin21@gmail.com>
2024-10-20 13:10:40 +0530

    add new design for unauthorized module

--- frontend/src/components/Unauthorized.jsx
+++ frontend/src/components/Unauthorized.jsx
@@ -1,9 +1,17 @@
-const Unauthorized = () => {
+import { AlertTriangle } from "lucide-react";
+import "../style/Unauthorized.css"; // Import the CSS file
+
+export default function Unauthorized() {
   return (
-    <div style={{border:'1px solid black',height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",userSelect:"none"}}>
-        <img style={{height:"65%",width:"60%"}} src="404.png"/>
+    <div className="unauthorized-container">
+      <div className="unauthorized-content">
+        <AlertTriangle className="icon" />
+        <h1 className="heading">Unauthorized Access</h1>
+        <p className="description">
+          Sorry, you don't have permission to access this page. Please contact
+          your administrator if you believe this is an error.
+        </p>
+      </div>
     </div>
   );
-};
-
-export default Unauthorized;
+}
